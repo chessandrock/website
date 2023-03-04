395bc730cf0d4a329d4c3bf35295d798b74f97d0
Rodrigo Fuentealba Cartes <chess.and.rock+github@gmail.com>
2023-03-04 04:20:33 -0300

    Added icons and fixed a couple texts.

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import profile from '../../assets/profile.jpg'
+import facebook from '../../assets/icons/facebook.svg'
+import instagram from '../../assets/icons/instagram.svg'
+import lichess from '../../assets/icons/lichess.svg'
 
 export const Home = () => {
   return (
@@ -11,9 +16,20 @@ export const Home = () => {
                 ¡Hola!
             </h1>
             <p className='text-base text-normal text-center font-sans mt-4'>
-                Mi nombre es Rodrigo Fuentealba Cartes y soy un ser humano neurodisperso que trabaja
-                en estadística y ciencia de datos, juega ajedrez y viaja en motocicleta.
+                Mi nombre es Rodrigo y soy un ser humano neurodisperso que trabaja
+                en ciencia de datos, juega ajedrez y viaja en motocicleta.
             </p>
+            <div className='flex flex-row items-center content-evenly mt-4'>
+                <a href="" target='_blank'>
+                    <img src={facebook} alt="" className='w-8' />
+                </a>
+                <a href="" target='_blank'>
+                    <img src={instagram} alt="" className='w-8' />
+                </a>
+                <a href="" target='_blank'>
+                    <img src={lichess} alt="" className='w-8' />
+                </a>
+            </div>
         </div>
     </div>
   )
